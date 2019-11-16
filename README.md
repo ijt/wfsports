c44b67357bce733a48c127dcef84b4357c44c304
Issac Trotts <issac.trotts@gmail.com>
2019-11-15 18:31:52 -0800

    add readme and first draft of program

---
+++ README.md
@@ -0,0 +1,24 @@
+# wfsports: WeFunder Sports challenge
+
+Start by running
+```
+$ wfsports start names.csv
+```
+This outputs a file called round1.csv.
+Each row of this file has the names of two players competing in this round.
+As the organizer, you get to add the name of the winner to each of the rows
+once the round is done. If the number of players isn't a power of two,
+there will be some players who get a free pass to go to the next round.
+They will show up in rows like `Alice,Alice,Alice`, meaning that Alice
+has played against her self and won.
+
+After entering the winners in round1.csv, run
+```
+$ wfsports next round1.csv
+```
+That will make random pairs of winners from round 1 and output round2.csv
+in the same format as round1.csv.
+Once they play each other, you as the organizer get to add the winners'
+names as in round1, and repeat running `wfsports next round2.csv` and
+repeat the process until someone wins the whole tournament.
+
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"csv"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 2 {
+		fmt.Println(`usages: 
+
+1. wfsports start names.csv
+2. wfsports next roundN.csv
+
+The CSV files do not have headers.
+names.csv has only a single column: the names of the players.
+roundN.csv has three columns: player1,player2,winner.
+`)
+		os.Exit(1)
+	}
+
+	command := flag.Arg(0)
+	filename := flag.Arg(1)
+	if err := run(command, filename); err != nil {
+		fmt.Printf("wfsports: error: ", err)
+		os.Exit(1)
+	}
+}
+
+func run(command, filename string) error {
+	switch command {
+	case "start":
+		return start(filename)
+
+	case "next":
+		return next(filename)
+
+	default:
+		return fmt.Errorf("unrecognized command: %s, want start or next", command)
+	}
+}
+
+func start(filename string) error {
+	recs, err := getRecords(filename)
+	if err != nil {
+		return errors.Wrapf(err, "getting records")
+	}
+
+	// Randomize order of the names by making them the keys of a map.
+	namesMap := make(map[string]bool)
+	for _, r := range recs {
+		namesMap[r[0]] = true
+	}
+	var names []string
+	for name := range namesMap {
+		names = append(names, name)
+	}
+
+	if err := generateRoundFile("round1.csv", names); err != nil {
+		return errors.Wrap(err, "generating round file")
+	}
+}
+
+var roundRx = regexp.MustCompile(`round(\d+).csv`)
+
+func next(doneRoundFilename string) error {
+	recs, err := getRecords(doneRoundFilename)
+	if err != nil {
+		return errors.Wrapf(err, "getting records")
+	}
+
+	// Get the winners of the done round, and randomize their order by
+	// making them the keys of a map.
+	namesMap := make(map[string]bool)
+	for _, r := range recs {
+		namesMap[r[2]] = true
+	}
+	var names []string
+	for name := range namesMap {
+		names = append(names, name)
+	}
+
+	m := roundRx.FindStringSubmatch(doneRoundFilename)
+	if len(m) != 2 {
+		return fmt.Errorf("given filename %s failed to match pattern %s", doneRoundFilename, roundRx)
+	}
+	k, err := strconv.Atoi(m[1])
+	if err != nil {
+		return fmt.Errorf("given filename %s has an integer part that somehow doesn't parse (%v). This is a bug.", doneRoundFilename, err)
+	}
+
+	roundFilename := fmt.Sprintf("round%d.csv", k+1)
+	if err := generateRoundFile(roundFilename, names); err != nil {
+		return errors.Wrap(err, "generating round file")
+	}
+
+}
+
+func generateRoundFile(roundFilename string, names []string) error {
+	roundFile, err := os.Create(roundFilename)
+	if err != nil {
+		return errors.Wrap(err, "creating round file")
+	}
+	defer roundFile.Close()
+	n := roundDownToPowerOfTwo(len(names))
+	// Output pairings so that n players are playing.
+	for i := 0; i < n; i += 2 {
+		fmt.Fprintf(roundFile, "%s,%s,\n", names[i], names[i+1])
+	}
+
+	// Output trivial matches where the remaining players play against themselves and win.
+	for i := n; i < len(names); i++ {
+		fmt.Fprintf(roundFile, "%s,%s,%s\n", names[i], names[i], names[i])
+	}
+}
+
+// roundDownToPowerOfTwo finds the closest power of 2 below n.
+func roundDownToPowerOfTwo(k int) int {
+	return int(math.Pow(2.0, math.Floor(math.Log2(float64(k)))))
+}
+
+func getRecords(filename string) ([][]string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return errors.Wrap(err, "opening CSV file")
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	return r.ReadAll()
+}
